69f796292bca9b0b86c35edacade600ce140a3ee
Yosh31 <adrianaudie@student.ub.ac.id>
2023-06-07 19:17:23 +0700

    Update Profile Lib Glide

--- app/src/main/java/com/example/proyekpamnote/EditProfile.java
+++ app/src/main/java/com/example/proyekpamnote/EditProfile.java
@@ -4,28 +4,50 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.provider.MediaStore;
 import android.text.TextUtils;
 import android.text.method.PasswordTransformationMethod;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.example.proyekpamnote.R;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.InputStream;
+import java.util.UUID;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class EditProfile extends AppCompatActivity implements View.OnClickListener{
 
+    private static final int REQUEST_IMAGE_PICK = 1;
     Button btnBackEdit, btnUpdateProfile;
     EditText etNama, etUsername,etEmail, etPassword;
     ImageButton btnUpdateImage;
@@ -110,7 +132,105 @@ public class EditProfile extends AppCompatActivity implements View.OnClickListen
         finish();
     }
     private void updateImage() {
+        // Create an intent to open the image gallery
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, REQUEST_IMAGE_PICK);
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        if (requestCode == REQUEST_IMAGE_PICK && resultCode == RESULT_OK && data != null) {
+            Uri selectedImageUri = data.getData();
+            uploadSelectedImage(selectedImageUri);
+        }
+    }
+
+    private void uploadSelectedImage(Uri imageUri) {
+        Log.d("ImageUri", imageUri.toString());
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        // Create a storage reference to the cloud storage
+        StorageReference storageRef = FirebaseStorage.getInstance().getReference();
+        // Generate a unique ID for the uploaded file
+        String fileId = UUID.randomUUID().toString();
+        // Create a storage reference with the unique ID as the filename
+        StorageReference imageRef = storageRef.child("images/" + fileId);
+
+        // Upload the image file to Firebase Storage
+        imageRef.putFile(imageUri)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        // Get the download URL of the uploaded image
+                        imageRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                            @Override
+                            public void onSuccess(Uri downloadUri) {
+                                // Handle the download URL of the uploaded image here
+                                String imageUrl = downloadUri.toString();
+//                                btnUpdateImage.setImageURI(downloadUri);
+                                Log.d("downloadUri", imageUrl);
+
+                                // Declaring executor to parse the URL
+                                ExecutorService executor = Executors.newSingleThreadExecutor();
+
+                                // Once the executor parses the URL
+                                // and receives the image, handler will load it
+                                // in the ImageView
+                                Handler handler = new Handler(Looper.getMainLooper());
+
+                                // Initializing the image
+                                final Bitmap[] image = {null};
+
+                                // Only for Background process (can take time depending on the Internet speed)
+                                executor.execute(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        // Image URL
+                                        String imageURL = downloadUri.toString();
+                                        // Tries to get the image and post it in the ImageView
+                                        // with the help of Handler
+                                        try {
+                                            InputStream in = new java.net.URL(imageURL).openStream();
+                                            image[0] = BitmapFactory.decodeStream(in);
+                                            // Only for making changes in UI
+                                            handler.post(new Runnable() {
+                                                @Override
+                                                public void run() {
+//                                                    btnUpdateImage.setImageBitmap(image[0]);
+
+                                                    Glide.with(getApplicationContext())
+                                                            .load(imageURL)
+                                                            .override(100, 100)
+                                                            .into(btnUpdateImage);
+                                                }
+                                            });
+                                        }
+                                        // If the URL does not point to
+                                        // an image or any other kind of failure
+                                        catch (Exception e) {
+                                            e.printStackTrace();
+                                        }
+                                    }
+                                });
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception exception) {
+                                Toast.makeText(EditProfile.this, "Failed to get download URL", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        Toast.makeText(EditProfile.this, "Failed to upload image", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
     private void updateProfile() {
 
         GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(this);
