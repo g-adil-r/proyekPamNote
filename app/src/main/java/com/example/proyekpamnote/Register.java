c4c185126cf0defef92ccabe7fc23f0076609ca8
hapisss313 <wahyudiansyah89@student.ub.ac.id>
2023-06-21 10:50:00 +0700

    update

--- app/src/main/java/com/example/proyekpamnote/Register.java
+++ app/src/main/java/com/example/proyekpamnote/Register.java
@@ -1,9 +1,13 @@
 package com.example.proyekpamnote;
 
-import android.annotation.SuppressLint;
-import android.app.ProgressDialog;
+import static android.content.ContentValues.TAG;
+
 import android.content.Intent;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -16,87 +20,120 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.UserProfileChangeRequest;
 
-public class Register extends AppCompatActivity {
-    private EditText editNama, editUsername, editEmail, editpassword;
-    private Button btnDaftar, btnMasuk;
-    ProgressDialog progressDialog;
+public class Register extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText editEmail, editPassword, editNama, editUsername;
+    private Button btnMasuk;
+    private Button btnDaftar;
     private FirebaseAuth mAuth;
 
-    public Register() {
-    }
-
-    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register);
-        editNama = findViewById(R.id.et_nama);
-        editUsername = findViewById(R.id.et_username);
+
         editEmail = findViewById(R.id.et_email);
-        editpassword = findViewById(R.id.et_password);
+        editPassword = findViewById(R.id.et_password);
         btnDaftar = findViewById(R.id.btn_daftar);
         btnMasuk = findViewById(R.id.btn_masuk1);
 
-        // Initialize Firebase Auth
         mAuth = FirebaseAuth.getInstance();
-        progressDialog = new ProgressDialog(Register.this);
-        progressDialog.setTitle("Loading");
-        progressDialog.setMessage("Silahkan tunggu");
-        progressDialog.setCancelable(false);
 
-
-        btnDaftar.setOnClickListener(v ->{
-            if(editNama.getText().length()>0 && editUsername.getText().length()>0 && editEmail.getText().length()>0 && editpassword.getText().length()>0){
-
-                daftar(editNama.getText().toString(), editUsername.getText().toString(), editEmail.getText().toString(), editpassword.getText().toString());
-            }else{
-                Toast.makeText(getApplicationContext(), "Silahkan isi semua data", Toast.LENGTH_SHORT).show();
-            }
-
-
-        });
-
+        btnDaftar.setOnClickListener(this);
+        btnMasuk.setOnClickListener(this);
 
     }
-    private void daftar(String nama, String Username, String email, String password){
-        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+
+
+    public void updateUI(FirebaseUser user) {
+        if (user != null) {
+            Intent intent = new Intent(Register.this, tampilNote.class);
+            startActivity(intent);
+        } else {
+            Toast.makeText(Register.this, "Log in First", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btn_masuk1:
+                login(editEmail.getText().toString(), editPassword.getText().toString());
+                break;
+            case R.id.btn_daftar:
+                signUp(editEmail.getText().toString(), editPassword.getText().toString());
+                break;
+            }
+        }
+
+    private void signUp(String email, String password) {
+        if (!validateForm()){
+            return;
+        }
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
-                if (task.isSuccessful() && task.getResult()!=null){
-                    FirebaseUser firebaseUser = task.getResult().getUser();
-                    if (firebaseUser!=null) {
-                        UserProfileChangeRequest request = new UserProfileChangeRequest.Builder()
-                                .setDisplayName(nama)
-                                .build();
-                        firebaseUser.updateProfile(request).addOnCompleteListener(new OnCompleteListener<Void>() {
-                            @Override
-                            public void onComplete(@NonNull Task<Void> task) {
-                                reload();
-                            }
-                        });
-                    }else {
-                        Toast.makeText(getApplicationContext(), "Pendaftaran akun gagal", Toast.LENGTH_SHORT).show();
-                    }
-                }else {
-                    Toast.makeText(getApplicationContext(), task.getException().getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                if (task.isSuccessful()) {
+                    Log.d(TAG, "createUserWithEmail:succes");
+                    FirebaseUser user = mAuth.getCurrentUser();
+                    updateUI(user);
+                    Toast.makeText(Register.this, user.toString(),Toast.LENGTH_SHORT).show();
+                } else {
+                    Log.d(TAG, "createUserWithEmail:failure", task.getException());
+                    Toast.makeText(Register.this, task.getException().toString(), Toast.LENGTH_SHORT).show();
+                    updateUI(null);
                 }
             }
         });
-
     }
 
-    private void reload(){
-        startActivity(new Intent(getApplicationContext(), tampilNote.class));
-    }
-    @Override
-    public void onStart() {
-        super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        if(currentUser != null){
-            reload();
+    private boolean validateForm() {
+        boolean result = true;
+        if (TextUtils.isEmpty(editEmail.getText().toString())) {
+            editEmail.setError("Required");
+            result = false;
+        } else {
+            editEmail.setError(null);
         }
+        if (TextUtils.isEmpty(editPassword.getText().toString())) {
+            editPassword.setError("Required");
+            result = false;
+        }else {
+            editPassword.setError(null);
+        }
+        return result;
+    }
+
+    public void login(String email, String password) {
+        if (!validateForm()){
+            return;
+        }
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()) {
+                    Log.d(TAG, "signInWithEmail:success");
+                    FirebaseUser user = mAuth.getCurrentUser();
+                    Toast.makeText(Register.this, user.toString(), Toast.LENGTH_SHORT).show();
+                    updateUI(user);
+                }else {
+                    Log.d(TAG, "signInWithEmail:failure", task.getException());
+                    Toast.makeText(Register.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                    updateUI(null);
+                }
+            }
+        });
+    }
+
+    private void login() {
     }
 }
+
